5138390691568e65bf240790ac7b0caa706f68e8
Cristian <cristianleguizamo31@gmail.com>
2025-10-08 20:39:02 -0500

    Integración springboot (back) y react (front), se conectó a una base de datos de railway

---
+++ Backend-API/secure-ticket/src/main/java/com/secure_ticket/SecureTicketApplication.java
@@ -0,0 +1,13 @@
+package com.secure_ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureTicketApplication.class, args);
+	}
+
+}
---
+++ Backend-API/secure-ticket/src/test/java/com/secure_ticket/SecureTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secure_ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
